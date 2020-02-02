7a811d9f94b9639fc3460b00088aa43141fdc8d0
Krystian <blocker92@o2.pl>
2020-02-02 23:32:52 +0100

    02.02.2020

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import Belgium from './Belgium';
-import Germany from './Germany';
-import Netherlands from './Netherlands';
-import France from './France';
+import Belgium from "./Belgium";
+// import Germany from './Germany';
+import Netherlands from "./Netherlands";
+// import France from "./France";
 
-const Main = (props) => {
-    const france = {
-        income: "Total brut",
-        paymentDay: "Paye Le"
-    };
-    const germany = {
-        income: "Gesamt-Brutto"
-    };
+const Main = props => {
+  const france = {
+    income: "Total brut",
+    paymentDay: "Paye Le"
+  };
+  const germany = {
+    income: "Gesamt-Brutto"
+  };
 
-    switch (props.countryData) {
-        case 'netherlands':
-            return <Netherlands />;
-        case 'belgium':
-            return <Belgium />;
-        case 'france':
-            return <France />;
-        case 'germany':
-            return <Germany />;
-    }
-}
+  switch (props.countryData) {
+    case "netherlands":
+      return <Netherlands />;
+    case "belgium":
+      return <Belgium />;
+    // case 'france':
+    // return <France />;
+    // case "germany":
+    //   return <Germany />;
+  }
+};
 
 export default Main;
